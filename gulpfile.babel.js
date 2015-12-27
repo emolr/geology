4462e966b03b2ca8ee197a5a93e5250f8444bac8
Emil Møller <emilrmoeller@gmail.com>
2015-12-28 00:55:29 +0100

    dist

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -176,7 +176,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['nunjucks', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
